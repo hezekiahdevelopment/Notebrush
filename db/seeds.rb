f1ee8a618c7c6facaf543af192baf22712316b47
Hezekiah Adedoyin <hezekiahdevelopment@gmail.com>
2017-01-01 10:37:48 -0500

    added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Hezekiah615", email: "Hezekiah615@gmail.com", password: "1h1h1h1H", password_confirmation: "1h1h1h1H")
+User.create(username: "wwafdash615", email: "Hesg24rh615@gmail.com", password: "1h1h1h1H", password_confirmation: "1h1h1h1H")
+User.create(username: "H23r2dah615", email: "Hez32efsfdsh615@gmail.com", password: "1h1h1h1H", password_confirmation: "1h1h1h1H")
+User.create(username: "yyvsf3ah615", email: "Hezekia32r2r3r@gmail.com", password: "1h1h1h1H", password_confirmation: "1h1h1h1H")
+User.create(username: "Geah615", email: "Hezekifsfsfs615@gmail.com", password: "1h1h1h1H", password_confirmation: "1h1h1h1H")
+p "Test users created"

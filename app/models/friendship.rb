2949a9316c51dde154101df4553adf347b5f7bdd
Hezekiah Adedoyin <hezekiahdevelopment@gmail.com>
2017-01-01 18:00:55 -0500

    added friendships

---
+++ app/models/friendship.rb
@@ -0,0 +1,19 @@
+class Friendship < ApplicationRecord
+	belongs_to :user
+	belongs_to :friend, class_name: "User"
+
+
+	def accept_friendship
+		self.update_attributes(state: "active", friended_at: Time.now)
+	end
+
+	def deny_friendship
+		self.destroy
+	end
+
+
+	def cancel_friendship
+		self.destroy
+	end
+
+end

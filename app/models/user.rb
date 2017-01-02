add3ad11e0170fea5d23734117e9ebe6509dbd00
Hezekiah Adedoyin <hezekiahdevelopment@gmail.com>
2017-01-01 19:29:48 -0500

    changed repo

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,16 @@ class User < ApplicationRecord
    def active_friends
    	self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:user)
    end
+
+   def friendship_status(user_2)
+   	friendship = Friendship.where(user_id: [self.id, user_2.id], friend_id: [self.id, user_2.id])
+   	unless friendship.any?
+   		return "not_friends"
+   	else
+   		if friendship.first.user == self
+   			return "pending"
+   		else
+   			return "requested"
+   		end
+   end
 end
